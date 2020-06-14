84e27dab8f794dedcad73249f75a982e2270e4e3
Jonathan Alpart <panjialang@gmail.com>
2020-06-14 18:46:50 -0500

    stats graphs api

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,9 @@ const router = require('express').Router();
 const Workout = require("../models/workout");
 
 router.get('/api/workouts', function (req, res) {
-    Workout.find()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
 });
 
 router.post('/api/workouts', function (req, res) {
-    Workout.create()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
 });
 
 router.get('/api/workouts/range', function (req, res) {
@@ -20,18 +14,8 @@ router.get('/api/workouts/range', function (req, res) {
 });
 
 router.post('/api/workouts/range', function (req, res) {
-    Workout.create()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
 });
 
 router.put('/api/workouts/:id', ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(
-        params.id,
-        { $push: { exercises: body } }
-    )
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
 });
-
 module.exports = router;
