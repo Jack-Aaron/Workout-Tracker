5d1d224e5b8711f66431c2cb5e14fd1cddf55195
Jonathan Alpart <panjialang@gmail.com>
2020-06-15 15:48:33 -0500

    fixed error in seed line 161

--- seeders/seed.js
+++ seeders/seed.js
@@ -159,7 +159,9 @@ let workoutSeed = [
         type: "resistance",
         name: "Bench",
         duration: 30,
-        distance: 2
+        weight: 250,
+        reps: 8,
+        sets: 5
       }
     ]
   }
