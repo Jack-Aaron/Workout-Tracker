468615299779df35f9da95aa5a9b4875c57dbd18
Jonathan Alpart <panjialang@gmail.com>
2020-06-15 15:36:39 -0500

    prettied up api routes

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,11 @@
 const router = require('express').Router();
 const db = require("../models");
 
-router.get("/api/workouts", (req,res) => {
+router.get("/api/workouts", (req, res) => {
     db.Workout.find()
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-  });
+        .then(dbWorkout => res.json(dbWorkout))
+        .catch(err => res.json(err));
+});
 
 router.put('/api/workouts/:id', ({ body, params }, res) => {
     db.Workout.findByIdAndUpdate(
