16e1c039cb4be7e48023a4e2e3516e8a5737f735
Jonathan Alpart <panjialang@gmail.com>
2020-06-15 16:33:42 -0500

    removed extraneous post route and added comments to api routes

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,30 @@
+// Dependencies
 const router = require('express').Router();
 const db = require("../models");
 
+// gets all workouts from database
 router.get("/api/workouts", (req, res) => {
     db.Workout.find()
         .then(dbWorkout => res.json(dbWorkout))
         .catch(err => res.json(err));
 });
 
+// gets range of workouts needed for our graphs
+router.get('/api/workouts/range', function (req, res) {
+    db.Workout.find()
+        .then(stats => res.json(stats))
+        .catch(err => res.json(err))
+});
+
+// gets a specific workout by ID
+router.get('api/workouts/:id', function ({ params }, res) {
+    db.Workout.findById(params.id) // specifically gets the id from the request
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
+});
+
 router.put('/api/workouts/:id', ({ body, params }, res) => {
-    db.Workout.findByIdAndUpdate(
+    db.Workout.findByIdAndUpdate( // searches by ID and also updates all in one method
         params.id,
         { $push: { exercises: body } }
     )
@@ -16,32 +32,14 @@ router.put('/api/workouts/:id', ({ body, params }, res) => {
         .catch(err => res.json(err))
 });
 
+// posts a new workout to the database
 router.post('/api/workouts', function (req, res) {
     db.Workout.create({
+        // gets the date at the time of posting
         day: new Date().setDate(new Date().getDate())
     })
         .then(data => res.json(data))
         .catch(err => res.json(err))
 });
-
-router.get('/api/workouts/range', function (req, res) {
-    db.Workout.find()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
-});
-
-router.post('/api/workouts/range', function (req, res) {
-    db.Workout.create()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
-});
-
-router.get('api/workouts/:id', function ({ params }, res) {
-    db.Workout.findById(params.id)
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
-});
-
-
 
 module.exports = router;
