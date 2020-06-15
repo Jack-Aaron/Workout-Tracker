d3ed15c3092b0659836de1c827e1b5994a9479e0
Jonathan Alpart <panjialang@gmail.com>
2020-06-15 15:20:59 -0500

    api routes confirmed

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,51 @@
 const router = require('express').Router();
-const Workout = require("../models/workout");
+const db = require("../models");
 
-router.get('/api/workouts', function (req, res) {
+router.get("/api/workouts", (req,res) => {
+    db.Workout.find()
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } }
+    )
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
 });
 
 router.post('/api/workouts', function (req, res) {
+    db.Workout.create({
+        day: new Date().setDate(new Date().getDate())
+    })
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
 });
 
 router.get('/api/workouts/range', function (req, res) {
-    Workout.find()
+    db.Workout.find()
         .then(data => res.json(data))
         .catch(err => res.json(err))
 });
 
 router.post('/api/workouts/range', function (req, res) {
+    db.Workout.create()
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
 });
 
-router.put('/api/workouts/:id', ({ body, params }, res) => {
+router.get('api/workouts/:id', function ({ params }, res) {
+    db.Workout.findById(params.id)
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
 });
+
+
+
 module.exports = router;
